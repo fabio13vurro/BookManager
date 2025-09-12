0526fad9fcb6e2e7a666b8e199c419a515781771
Java <fabiovurro46@gmail.com>
2025-09-12 12:50:58 +0200

    add author/book/category entity, repository, model | add generics repository and interfaces

---
+++ BookManager.DAL/BookDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BookManager.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManager.DAL
+{
+    public class BookDbContext : DbContext
+    {
+        
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+           
+        }
+    }
+}
---
+++ BookManager.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManager.DAL.Interfaces
+{
+    public interface IRepository <T> where T : class
+    {
+        T GetById(int id);
+
+        IEnumerable<T> GetAll();
+
+        void Insert(T entity);
+
+        void Update (T entity);
+
+        void Delete (int id);
+
+        void SaveChanges ();
+
+    }
+}
